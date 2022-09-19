79306e0734a12070d2abf31316c502438155c0b8
Farhany <lanichlanich@gmail.com>
2022-09-19 08:31:02 +0700

    promises tugas farhany

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emosi) => {
+  try {
+    const dataEmosiTheaterIXX = await promiseTheaterIXX();
+    const dataEmosiTheaterVGC = await promiseTheaterVGC();
+
+    const dataCountTheaterVGC = dataEmosiTheaterVGC.filter((res) => res.hasil === emosi);
+    const dataCountTheaterIXX = dataEmosiTheaterIXX.filter((res) => res.hasil === emosi);
+    return dataCountTheaterVGC.length + dataCountTheaterIXX.length;
+  } catch (error) {}
+};
 
 module.exports = {
   promiseOutput,
